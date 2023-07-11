678c53c613edfed7b21af44104ee1ce2d56ae86c
logic <1758555877@qq.com>
2023-07-11 21:20:17 +0800

    尝试添加模拟键盘

---
+++ src/components/keybord.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="keyboard" v-show="showKeyboard">
+            <div class="row">
+                <button @click="addNumber(1)">1</button>
+                <button @click="addNumber(2)">2</button>
+                <button @click="addNumber(3)">3</button>
+            </div>
+            <div class="row">
+                <button @click="addNumber(4)">4</button>
+                <button @click="addNumber(5)">5</button>
+                <button @click="addNumber(6)">6</button>
+            </div>
+            <div class="row">
+                <button @click="addNumber(7)">7</button>
+                <button @click="addNumber(8)">8</button>
+                <button @click="addNumber(9)">9</button>
+            </div>
+            <div class="row">
+                <button @click="addNumber(0)">0</button>
+                <button @click="hideKeyboard">Done</button>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "KeyBord",
+    data() {
+        return {
+        };
+    },
+};
+</script>
+  
+<style>
+</style>
+  
--- src/vrify/vrify.vue
+++ src/vrify/vrify.vue
@@ -10,29 +10,40 @@
                 </h1>
             </div>
             <div class="inputnumber">
-                <div></div>
-                <div>
-                    <input v-validate="'required|credit_card'" name="password" type="text" placeholder="输入的内容格式是信用卡的号码格式">
-
+                <div class="svg-width">
+                    <img src="../assets/svg/phone.svg" style="height: 40px;width: 40px;" />
+                </div>
+                <div class="input">
+                    <input v-validate="'required|credit_card'" name="password" type="text" placeholder="Phone">
                 </div>
             </div>
+            <div class="continue">
+                <button>
+                    CONTINUE
+                </button>
+            </div>
+            <Keyboard />
         </div>
     </div>
 </template>
   
 <script>
+import Keyboard from "../components/keybord.vue";
 export default {
     name: "VriFy",
     data() {
         return {
         };
     },
+    components: {
+        Keyboard
+    },
     methods: {
-        back() {
-            this.$router.push({
-                path: "/signin",
-            });
-        },
+        // back() {x
+        //     this.$router.push({
+        //         path: "/signin",
+        //     });
+        // },
     },
 };
 </script>
@@ -52,7 +63,7 @@ export default {
 }
 
 .vrify-contant {
-    padding: 36px 0 42px 0;
+    padding: 45px 0 60px 0;
 }
 
 .vrify-contant h1 {
@@ -62,6 +73,61 @@ export default {
     letter-spacing: 0;
     margin: 0;
     font-weight: 200;
+}
 
+.svg-width {
+    height: 40px;
+    width: 60px;
+}
+
+.input {
+    width: 80%;
+    height: 60px;
+}
+
+.input {
+    position: relative;
+    right: 52px;
+}
+
+.inputnumber {
+    display: flex;
+    justify-content: flex-start;
+    padding: 30px 0 30px 0;
+}
+
+.inputnumber input {
+    background: #0E0B1F;
+    border: none;
+    border-bottom: 2px solid #dbdbdb;
+    border-top: 0px;
+    border-left: 0px;
+    border-right: 0px;
+    outline: none;
+    color: #fff;
+    opacity: 0.7;
+    padding-left: 40px;
+    font-family: Helvetica;
+    font-size: 14px;
+    letter-spacing: 0;
+    line-height: 22px;
+}
+
+.inputnumber input {
+    width: 95%;
+    height: 40px;
+}
+
+.continue {
+    padding: 20px 0 330px 0;
+}
+
+.continue button {
+    background: #CBFB5E;
+    border: none;
+    padding: 14px;
+    border-radius: 3px;
+    width: 100%;
+    border: none;
 }
 </style>
