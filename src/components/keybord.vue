55e6f0f9733c793109bc49c5afa113778a79ab15
logic <1758555877@qq.com>
2023-07-12 17:36:04 +0800

    修改内容

--- src/components/keybord.vue
+++ src/components/keybord.vue
@@ -1,43 +1,41 @@
 <template>
-    <div>
-        <div class="keyboard">
-            <div class="row">
-                <button class="number" @click="addNumber()">1</button>
-                <button class="number" @click="addNumber()">2
-                    <h1>ABC</h1>
-                </button>
+    <div class="keyboard">
+        <div class="row">
+            <button class="number" @click="addNumber()">1</button>
+            <button class="number" @click="addNumber()">2
+                <h1>ABC</h1>
+            </button>
 
-                <button class="number" @click="addNumber()">3
-                    <h1>DEF</h1>
-                </button>
+            <button class="number" @click="addNumber()">3
+                <h1>DEF</h1>
+            </button>
 
-            </div>
-            <div class="row">
-                <button class="number" @click="addNumber()">4
-                    <h1>GHI</h1>
-                </button>
-                <button class="number" @click="addNumber()">5
-                    <h1>JKL</h1>
-                </button>
-                <button class="number" @click="addNumber()">6
-                    <h1>MNO</h1>
-                </button>
-            </div>
-            <div class="row">
-                <button class="number" @click="addNumber()">7
-                    <h1>PGRS</h1>
-                </button>
-                <button class="number" @click="addNumber()">8
-                    <h1>TUV</h1>
-                </button>
-                <button class="number" @click="addNumber()">9
-                    <h1>WXYZ</h1>
-                </button>
-            </div>
-            <div class="row ceanter">
-                <button @click="addNumber()" class="zero number">0</button>
-                <img src="../assets/svg/backdelete.svg" class="backdelete" />
-            </div>
+        </div>
+        <div class="row">
+            <button class="number" @click="addNumber()">4
+                <h1>GHI</h1>
+            </button>
+            <button class="number" @click="addNumber()">5
+                <h1>JKL</h1>
+            </button>
+            <button class="number" @click="addNumber()">6
+                <h1>MNO</h1>
+            </button>
+        </div>
+        <div class="row">
+            <button class="number" @click="addNumber()">7
+                <h1>PGRS</h1>
+            </button>
+            <button class="number" @click="addNumber()">8
+                <h1>TUV</h1>
+            </button>
+            <button class="number" @click="addNumber()">9
+                <h1>WXYZ</h1>
+            </button>
+        </div>
+        <div class="row ceanter">
+            <button @click="addNumber()" class="zero number">0</button>
+            <img src="../assets/svg/backdelete.svg" class="backdelete" />
         </div>
     </div>
 </template>
@@ -58,20 +56,19 @@ export default {
     justify-content: space-around;
 }
 
-.number  {
-    height: 46px;
+.number {
     background: #444444;
-    box-shadow: 0 1px 0 0 #000000;
-    font-family: Helvetica;
     font-size: 25px;
     color: #FFFFFF;
-    letter-spacing: 0;
-    line-height: 30px;
     font-weight: 300;
     border: none;
-    width: 117px;
     margin: 12px 0 0 11px;
     border-radius: 5px;
+}
+
+.number {
+    height: 46px;
+    width: 90px;
 }
 
 .ceanter {
@@ -88,7 +85,7 @@ export default {
     margin: 0;
     font-size: 7px;
     font-weight: 300;
-    margin-top: -13px;
+    margin-top: -6px;
     padding-top: 4px;
 }
 
